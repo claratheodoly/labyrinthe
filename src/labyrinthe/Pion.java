a1c7a281b1c1ad87c544bf7cd8e3f49a37fde10d
e10e3 <emile.royer@pm.me>
2020-12-01 14:46:15 +0100

    Mise en placede la classe Pion
    Elle est plutôt simple.

--- src/labyrinthe/Pion.java
+++ src/labyrinthe/Pion.java
@@ -10,5 +10,16 @@ package labyrinthe;
  * @author Lucas DELMOTTE, Clara THEODOLY et Émile ROYER
  */
 public class Pion {
+	
+	Joueur propriétaire;
+	
+	/**
+	 * Crée un pio lié à un joueur
+	 * 
+	 * @param joueur Le propriétaire du pion
+	 */
+	Pion(Joueur joueur) {
+		propriétaire = joueur;
+	}
 
 }
