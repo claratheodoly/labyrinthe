266b7277eae2b1a90874b91f9d6a20cc6863ef15
e10e3 <emile.royer@pm.me>
2020-12-01 15:04:27 +0100

    Ajout du squellette de Joueur
    Ajout des attributs, du constructeur et des méthodes (vides)

--- src/labyrinthe/Joueur.java
+++ src/labyrinthe/Joueur.java
@@ -6,10 +6,33 @@
 package labyrinthe;
 
 /**
- * Ipmlémente les joueurs
+ * Implémente les joueurs
  *
  * @author Lucas DELMOTTE, Clara THEODOLY et Émile ROYER
  */
 public class Joueur {
-
+	
+	String nom;
+	String couleur;
+	String [] listeObjets;
+	Carte [] listeCartes;
+	int indexCarteRetournee;
+	Pion marqueur;
+	
+	Joueur(String nom_joueur, int nombre_cartes) {
+		nom = nom_joueur;
+		listeObjets = new String [nombre_cartes];
+		listeCartes = new Carte[nombre_cartes];
+		indexCarteRetournee = 0;
+		marqueur = new Pion(this);
+	}
+	
+	public boolean rammaserObjet(Tuile tuile) {
+		return false;
+	}
+	
+	public boolean ajouterCarte(Carte carte) {
+		return false;
+	}
+	
 }
