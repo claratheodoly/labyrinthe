41567ec3ec4ea8b0db722a430667f390e588d6b3
e10e3 <emile.royer@pm.me>
2020-12-01 15:15:22 +0100

    Ajout du squellette de Plateau
    Ajout des attributs, du constructeur et des méthodes (vides)

--- src/labyrinthe/Plateau.java
+++ src/labyrinthe/Plateau.java
@@ -12,5 +12,45 @@ package labyrinthe;
  * @author Lucas DELMOTTE, Clara THEODOLY et Émile ROYER
  */
 public class Plateau {
-
+	
+	Tuile [][] tuiles;
+	Tuile tuileVolante;
+	
+	Plateau() {
+		tuiles = new Tuile[7][7];
+		tuileVolante = new Tuile();
+	}
+	
+	public boolean pontEntreTuiles(int x1, int y1, int x2, int y2) {
+		return false;
+	}
+	
+	public boolean cheminPossible(int x1, int y1, int x2, int y2) {
+		return false;
+	}
+	
+	public boolean deplacerColonne(int num_col, boolean dir) {
+		return false;
+	}
+	
+	public boolean deplacerLigne(int num_col, boolean dir) {
+		return false;
+	}
+	
+	public boolean placerTuile(int x, int y) {
+		return false;
+	}
+	
+	public boolean deplacerPion(int num_col, boolean dir) {
+		return false;
+	}
+	
+	public int[] positionPion(Joueur proprietaire) {
+		return new int[0];
+	}
+	
+	public boolean intervertirTuiles(int x1, int y1, int x2, int y2) {
+		return false;
+	}
+	
 }
