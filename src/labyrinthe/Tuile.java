3ff4304fb4d124eed6932dc2126a037a20495811
e10e3 <emile.royer@pm.me>
2020-12-11 23:53:53 +0100

    Réparation de la calsse Tuile.
    Ajout de méthodes et retrait d'erreurs

--- src/labyrinthe/Tuile.java
+++ src/labyrinthe/Tuile.java
@@ -11,7 +11,6 @@ package labyrinthe;
  */
 public class Tuile {
 
-	// Tuile(String) tournerTuile(int) : boolean placerObjet(String) : boolean recupererObjet(Pion) : boolean
 	String type;
 	int orientation;
 	String objet;
@@ -25,43 +24,57 @@ public class Tuile {
 		type = type_donne;
 		orientation = 0;
 	}
-        /*
-	public RotatingImage(BufferedImage sourceImage){
-		
-		
-	}
-	public boolean tournerTuile(int degres) {
-		if (une_orientation = 90){ // rotation de la tuile de pi/2
-            		rotatingImage.setAngle(Math.PI*1/2);
-      
-        }
-        	if (une_orientation = 180){
-            		rotatingImage.setAngle(Math.PI*2/2);
-      
-        }
-        	if (une_orientation = 270){
-            		rotatingImage.setAngle(Math.PI*3/2);
-      
-        }
-        	if (une_orientation = 0){
-            		rotatingImage.setAngle(Math.PI*4/2);
-      
-        }
+
+	public void RotatingImage(String sourceImage) {
+		// TODO
 	}
 
-	public boolean placerObjet(String objet) {
-		if (objet == true) { 
-            return false;  
-        } else {
-            objet = true;
-            return true;
+	public boolean tournerTuile(int degres) {
+		/*
+		if (orientation != 90) { // rotation de la tuile de pi/2
+			RotatingImage("a").setAngle(Math.PI * 1 / 2);
+
+		}
+		if (une_orientation = 180) {
+			rotatingImage.setAngle(Math.PI * 2 / 2);
+
+		}
+		if (une_orientation = 270) {
+			rotatingImage.setAngle(Math.PI * 3 / 2);
+
+		}
+		if (une_orientation = 0) {
+			rotatingImage.setAngle(Math.PI * 4 / 2);
+
+		}*/
+		return true;
+	}
+
+	public boolean placerObjet(String objetAAjouter) {
+		if (objet != null) {
+			return false;
+		} else {
+			objet = objetAAjouter;
+			return true;
+		}
 	}
 
 	public boolean recupererObjet(Pion pion_joueur) {
-		Objet ObjetRecuperer = ObjetActuel;
-        ObjetActuel = null;
-        return ObjetRecuperer; 
-		
+		if (objet == null) {
+			return false;
+		}
+		pion_joueur.propriétaire.rammaserObjet(this);
+		objet = null;
+		return true;
+
 	}
-*/
+	
+	public boolean presenceObjet() {
+		return false;
+	}
+	
+	public boolean presencePion() {
+		return false;
+	}
+
 }
