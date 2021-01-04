cc4ce2d12d12fb21fde984a8d8f011335217f4d7
e10e3 <emile.royer@pm.me>
2021-01-04 01:20:53 +0100

    Ce n'était pas une erreur

--- src/labyrinthe/Joueur.java
+++ src/labyrinthe/Joueur.java
@@ -41,7 +41,7 @@ public class Joueur {
 	public void nouvellePosition(Tuile nouvelleTuile) {
 		if (nouvelleTuile.presenceObjet()) {
 			if (listeCartes[indexCarteRetournee].nomObjet.equals(nouvelleTuile.type)) {
-				rammasserObjet(nouvelleTuile);
+				ramasserObjet(nouvelleTuile);
 				indexCarteRetournee++;
 			}
 		}
